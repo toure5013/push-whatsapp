fea2e13a06048979c366aeab0474acaf1633f882
SOULEYMANE TOURE <toure5013@gmail.com>
2022-06-15 10:20:06 +0000

    first template

---
+++ Test/message_call_to_actions.js
@@ -0,0 +1,102 @@
+const axios = require("axios").default;
+const productToken = "541fea0e-3ab9-404d-8d9a-07a6bf09a185"
+const djingo_whatsapp_number = "2250769313109"
+const users_number = require("./user_number")
+
+// TEMPLATE DATA
+const template_name = "orange_et_moi"
+const namespace = "0a72eb69_2478_4109_a8b6_577bf0ec778a"
+
+
+
+async function push_djingo_whastapp(_user_number) {
+
+
+  var data ={
+    "messages": {
+        "authentication": {
+            "producttoken": productToken
+        },
+        "msg": [{
+            "from": djingo_whatsapp_number,
+            "to": [{
+                    "number":  _user_number
+                }
+            ],
+            "body": {
+                "type": "auto",
+                "content": "Reviens sur Orange et moi, achète ton pass, et remporte 100% de bonus. Rendez-vous sur https://bit.ly/3HxDfMG"
+            },
+            "allowedChannels": ["WhatsApp"],
+            "richContent": {
+                "conversation":
+                [{
+                    "template": {
+                        "whatsapp": {
+                            "namespace": namespace,
+                            "element_name": template_name,
+                            "language": {
+                                "policy": "deterministic",
+                                "code": "fr"
+                            },
+                            "components": [{
+                                "type": "body",
+                                "parameters": [
+                                  
+                                ]
+                            }]
+                        }
+                    }
+                }]
+            }
+        }]
+    }
+}
+
+
+
+
+  var config = {
+    method: 'post',
+    url: 'https://gw.cmtelecom.com/v1.0/message',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    data: data
+  };
+
+  try {
+    const push_return = await axios(config);
+    console.log(push_return.data)
+  } catch (error) {
+      console.log('error API');
+      console.log(error);
+  }
+
+}
+
+
+
+async function send_to_number() {
+    // console.log(users_number.lot1);
+    console.log(users_number.lot1[0]);
+ 
+    try {
+        for (let i = 0; i < users_number.lot1.length; i++) {
+            if(i==1){
+                await push_djingo_whastapp("2250708175631")
+            }
+            // await push_djingo_whastapp( users_number.lot1[i])
+            console.log(`done - ${i}`);
+        }
+    } catch (error) {
+        console.log(e)
+    }
+   
+    console.log("done ---- final");
+}
+  
+  
+  
+  
+  send_to_number()
